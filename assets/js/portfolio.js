650da118ed2ec06558352b605f19dbb2656bce3d
Kraig Kowalski <kwkowalski@yahoo.com>
2018-07-03 02:00:23 -0400

    Update All Files

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,13 +1,12 @@
-$('.dropdown-toggle').click(changeClass);
-
-function changeClass(){
-	$('.dropdown-menu').toggleClass('dropdown-menu-show');
-}
-
-
 $('.navbar-toggler').click(hamburgerToggle);
 
 function hamburgerToggle(){
 	$('.navbar-collapse').toggleClass('show');
 	$('.dropdown-menu').toggleClass('hidden');
 }
+
+$('#colorGame').hover(function(){
+	$(this).addClass("back-face");
+	}, function(){
+	$(this).removeClass("back-face");
+});
