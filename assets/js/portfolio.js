6531860ae685c5bc79d9a69aa06922662b9f6c23
Kraig Kowalski <kwkowalski@yahoo.com>
2018-07-05 23:42:54 -0400

    Update Active Links

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -4,3 +4,10 @@ function hamburgerToggle(){
 	$('.navbar-collapse').toggleClass('show');
 	$('.dropdown-menu').toggleClass('hidden');
 }
+
+$('.nav-item').click(activeLink);
+
+function activeLink(){
+	$('.nav-item').removeClass('active');
+	$(this).addClass('active');
+}
